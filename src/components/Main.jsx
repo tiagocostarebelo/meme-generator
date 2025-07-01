e624a39a539c787444ef63829f34cb2edf8ae99b
Tiago Costa Rebelo <43050965+tiagocostarebelo@users.noreply.github.com>
2025-07-01 13:32:03 +0000

    initial commit with initial state set

---
+++ src/components/Main.jsx
@@ -0,0 +1,38 @@
+import {useState} from "react"
+
+export default function Main() {
+
+    const [meme, setMeme] = useState({
+        "topText": "One does not simply",
+        "bottomText": "Walk into Mordor",
+        "imgUrl": "http://i.imgflip.com/1bij.jpg"
+    })
+
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imgUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
